e5e5d8f089e75411537321a7427bb8525cedc81b
Prateem Shrestha <prateems@gmail.com>
2014-11-21 04:49:51 -0500

    Worked on Path. Added validation, navigation, and other utility methods.

--- src/system/utilities/Path.java
+++ src/system/utilities/Path.java
@@ -1,10 +1,16 @@
 package system.utilities;
 
+import exceptions.FileSystemException;
 import system.Directory;
 import system.FileSystem;
+import system.FileSystemObject;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
- * TODO
+ * Handle Path-related tasks for the unique instance of JShell it is
+ * instantiated for.
  *
  * @author Prateem Shrestha
  * @see system.FileSystem
@@ -18,10 +24,17 @@ public class Path {
   private Directory workingDirectory;
 
   /**
-   * TODO
+   * A stack to keep track of working directory locations committed to memory.
+   */
+  private final Deque<String> pathStack;
+
+  /**
+   * Initialize by identifying the root of the system as the working directory.
+   * Prepare a path stack to store remembered locations in.
    */
   public Path() {
     workingDirectory = FileSystem.ROOT;
+    pathStack = new ArrayDeque<String>();
   }
 
   /**
@@ -31,6 +44,167 @@ public class Path {
    */
   public Directory getWorkingDirectory() {
     return workingDirectory;
+  }
+
+  /**
+   * Split a fully qualified path into the components of location and the name
+   * of the object at the end of the path. The first element of the returned
+   * array is the path to the Directory that should contain the desired object.
+   * The second element of the returned array is simply the name of said object.
+   *
+   * @param path The path to split.
+   * @return A two element array consisting of the fully qualified path string
+   * to the location of the object, and the object name.
+   */
+  public static String[] getPathComponents(String path) {
+    String[] pathNodes = path.split("/");
+    String[] output = new String[2];
+    output[1] = pathNodes[pathNodes.length - 1];
+
+    StringBuilder pathBuilder = new StringBuilder();
+    if (path.startsWith("/")) {
+      // Maintain absolute paths.
+      pathBuilder.append("/");
+    }
+
+    for (int i = 0; i < pathNodes.length - 1; i++) {
+      pathBuilder.append(pathNodes[i]);
+
+      // Don't terminate with a trailing slash.
+      // This is particularly important to prevent infinite looping in get().
+      if (i != pathNodes.length - 2) {
+        pathBuilder.append("/");
+      }
+    }
+
+    output[0] = pathBuilder.toString();
+    return output;
+  }
+
+  /**
+   * Return an absolute path to the current working directory.
+   *
+   * @return An absolute path to the current working directory.
+   */
+  public final String getAbsolutePath() {
+    Directory directory = workingDirectory;
+    if (directory == FileSystem.ROOT) {
+      return "/";
+    }
+
+    // Set up a LIFO list of Directories in the path to this directory
+    // starting from this directory, working backwards, until the
+    // root directory is reached.
+    Deque<Directory> path = new ArrayDeque<Directory>();
+    while (directory != FileSystem.ROOT) {
+      path.push(directory);
+      directory = directory.getParent();
+    }
+
+    StringBuilder pathBuilder = new StringBuilder();
+    while (!path.isEmpty()) {
+      pathBuilder.append("/").append(path.pop());
+    }
+
+    return pathBuilder.toString();
+  }
+
+  /**
+   * Store the path to the current working directory in memory. Adds the current
+   * location to the path stack for later recall.
+   */
+  public final void rememberLocation() {
+    pathStack.push(getAbsolutePath());
+  }
+
+  /**
+   * Return true if there is 1 or more working directory locations in the path
+   * stack. Return false otherwise.
+   *
+   * @return True if and only if path stack has elements in it.
+   */
+  public final boolean hasStoredLocation() {
+    return !pathStack.isEmpty();
+  }
+
+  /**
+   * Return the most recently stored working directory location.
+   */
+  public final String getLastStoredLocation() {
+    return pathStack.pop();
+  }
+
+  /**
+   * Update the given path string to include the file system object specified.
+   *
+   * @param path The path string to update with the addition of the object.
+   * @param fso  The object to update into the string.
+   * @return The new String path containing the file system object.
+   */
+  public final String buildPathToObject(String path, FileSystemObject fso) {
+    return path + (path.endsWith("/") ? "" : "/") + fso;
+  }
+
+  /**
+   * Validate a path string. Evaluates whether or not a path string will lead to
+   * an existing FileSystemObject.
+   *
+   * @param type The FileSystemObject type to expect at the end of the path.
+   * @param path A path string to be validated.
+   * @return True if and only if a FileSystemObject of the specified subclass
+   * (Directory or File) is found by the given path string.
+   */
+  public final boolean isValid(Class<? extends FileSystemObject> type,
+                               String path) {
+    try {
+      FileSystemObject fileSystemObject = get(path);
+      if (fileSystemObject.getClass() == type) {
+        return true;
+      }
+    } catch (FileSystemException e) {
+      return false;
+    }
+    return false;
+  }
+
+  /**
+   * Get a FileSystemObject by a valid fully qualified path string.
+   *
+   * @param path A valid fully qualified path to a FileSystemObject.
+   * @return The FileSystemObject found at the given path.
+   * @throws FileSystemException If getting the
+   */
+  public final FileSystemObject get(String path) throws FileSystemException {
+    if (path.equals("/") || path.equals("~")) {
+      return FileSystem.ROOT;
+    }
+
+    // Find the Directory of the desired object.
+    Directory location = workingDirectory;
+    if (path.contains("/")) {
+      String[] pathComponents = getPathComponents(path);
+      location = (Directory) get(pathComponents[1]);
+      path = pathComponents[1];
+    }
+
+    if (path.equals(".")) {
+      return location;
+    } else if (path.equals("..")) {
+      return location.getParent();
+    }
+
+    return location.getChildByName(path);
+  }
+
+  /**
+   * Follow a specific path and update the working directory.
+   *
+   * @param path A verified path string to follow.
+   * @throws FileSystemException If attempting to follow the path resulted
+   * in an error, most likely from trying to change the working directory.
+   */
+  public final void goTo(String path) throws FileSystemException {
+    workingDirectory = (Directory) get(path);
   }
 
 }
